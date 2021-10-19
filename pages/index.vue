08b679bcdc60c9752571b86e1cc79f660147f098
shylabo <shuya.sohot@gmail.com>
2021-10-19 23:03:57 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>
+      Welcome to the best corny dad jokes app on the web
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome to the dad jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
